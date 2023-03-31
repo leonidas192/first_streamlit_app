f68260801cc37d7df86e4bf7533cf4beb4c07274
Crawlingflour <56856018+leonidas192@users.noreply.github.com>
2023-03-31 10:17:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.stop()
 streamlit.header("The fruit load list contains:")
 
 def get_fruit_load_list():
-  with my_cnx_cursor() as my_cur():
+  with my_cnx_cursor() as my_cur:
     my_cur.execute("SELECT * FROM fruit_load_list")
     return my_cur.fetchall()
 
@@ -54,7 +54,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
   
 def insert_row_snowflake():
-  with my_cnx.cursor() as my_cur():
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + new_fruit
   
